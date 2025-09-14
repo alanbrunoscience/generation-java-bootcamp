91c195795c5d42a6d5f9811544524e5a3a97c1e9
alanbrunoscience <alanbrunomath@gmail.com>
2025-09-14 14:45:32 -0300

    :sparkles: Add code samples

---
+++ java-lessons/src/lesson08/twodimensionalarray/Example01IntegersArray2D.java
@@ -0,0 +1,19 @@
+package lesson08.twodimensionalarray;
+
+import java.util.Arrays;
+
+public class Example01IntegersArray2D {
+
+	public static void main(String[] args) {
+
+		// Two-dimensional array declaration
+		int integers[][] = { { 10, 15, 35 }, { 70, 120, 140 }, { 50, 100, 150 } };
+		
+		// Print elements of the array
+		System.out.println(Arrays.deepToString(integers));
+
+		// Arrays.deepToString() → Its primary purpose is to return a string representation of the 
+		// "deep contents" of a specified array, particularly designed for handling multidimensional arrays.
+	}
+
+}
