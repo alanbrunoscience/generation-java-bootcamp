9c498a634e151ea1cbb23686bea3b813af354f39
alanbrunoscience <alanbrunomath@gmail.com>
2025-10-06 22:06:43 -0300

    :sparkles: Add code examples about Optional

---
+++ java-lessons/src/lesson18/optional/optionalmethods2/CollaboratorApp.java
@@ -0,0 +1,23 @@
+package lesson18.optional.optionalmethods2;
+
+import java.util.Optional;
+
+import lesson18.optional.optionalmethods2.model.Collaborator;
+
+public class CollaboratorApp {
+
+	public static void main(String[] args) {
+
+		Collaborator c1 = new Collaborator(1L, "James Bond", 1000.0f);
+
+		Optional<Collaborator> collaboratorOptional = Optional.of(c1);
+
+		collaboratorOptional.map(response -> response.getName())
+				.filter(collaboratorName -> collaboratorName.startsWith("J"))
+				.orElseThrow(() -> new RuntimeException("\n→ Collaborator not found!"));
+		
+		System.out.printf("→ Collaborator name: %s.%n", collaboratorOptional.get().getName());
+
+	}
+
+}
