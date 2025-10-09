718bf30612b656a83545ffffb818d8742bb026d1
alanbrunoscience <alanbrunomath@gmail.com>
2025-10-08 23:05:02 -0300

    :sparkles: Add code examples about the Math class

---
+++ java-lessons/src/lesson21/auxiliarycontent/mathclass/Example02MathCeil.java
@@ -0,0 +1,27 @@
+package lesson21.auxiliarycontent.mathclass;
+
+import java.util.Locale;
+
+public class Example02MathCeil {
+
+	/**
+	 * Math.ceil(x)
+	 * 
+	 * → Rounds 'x' to the SMALLEST INTEGER NOT LESS than 'x'. The argument 'x' must
+	 * be double.
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		double n1 = -9.8, n2 = 9.2;
+
+		System.out.println("=== Math.ceil(x) ===\n");
+		System.out.printf("→ 1) Math.ceil(%.2f) = %.2f;%n", n1, Math.ceil(n1)); // -9.00
+		System.out.printf("→ 2) Math.ceil(%.2f) = %.2f.%n", n2, Math.ceil(n2)); // 10.00
+
+	}
+
+}
