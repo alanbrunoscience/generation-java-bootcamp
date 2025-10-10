62b75ed89ad58e9c4ae66be8091c274b8d944165
alanbrunoscience <alanbrunomath@gmail.com>
2025-10-10 15:03:36 -0300

    :sparkles: Add code examples about the String class

---
+++ java-lessons/src/lesson19/auxiliarycontent/stringclass/Example06StringTrim.java
@@ -0,0 +1,24 @@
+package lesson19.auxiliarycontent.stringclass;
+
+public class Example06StringTrim {
+
+	/**
+	 * String.trim()
+	 * 
+	 * → Returns the String without all whitespace that appears at the beginning and
+	 * end of the String.
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		String word = " Trim test ";
+
+		System.out.println("=== String.trim() ===\n");
+		System.out.printf("→ 1) Word: '%s';%n", word); // " Trim test "
+		System.out.printf("→ 2) Removes whitespaces at the beginning and end of the String: '%s'.%n", word.trim()); // "Trim
+																													// test"
+
+	}
+
+}
