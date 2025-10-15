0f022b2b14bcea6c8ec444a73442cb313b4de148
alanbrunoscience <alanbrunomath@gmail.com>
2025-10-15 19:43:17 -0300

    :sparkles: Add code examples about the Period and Duration classes

---
+++ java-lessons/src/lesson19/auxiliaryclasses/dateandtime/Example06PeriodAndDurationClasses.java
@@ -0,0 +1,39 @@
+package lesson19.auxiliaryclasses.dateandtime;
+
+public class Example06PeriodAndDurationClasses {
+
+	/**
+	 * The 'Period' and 'Duration' Classes
+	 * 
+	 * → The 'Period' class represents a quantity of time in terms of years, months,
+	 * and days. The 'Period' class is widely used to modify the values of a
+	 * specific date or to get the difference between two dates.
+	 * 
+	 * → The 'Duration' class represents a quantity of time in terms of seconds and
+	 * nanoseconds (or milliseconds, depending on the context). The 'Duration' class
+	 * is widely used to modify the values of a specific time or to get the
+	 * difference between two times.
+	 * 
+	 * -----------------------------------------------------------------------------
+	 * 
+	 * ⚠️ Important
+	 * 
+	 * → Pay Attention to Detail when working with the 'Period' and 'Duration'
+	 * Classes. The 'Period' and 'Duration' Classes do not accept 'LocalDateTime'
+	 * Class Objects.
+	 * 
+	 * -----------------------------------------------------------------------------
+	 * 
+	 * → In the example below, we will see how to use the 'Period' and 'Duration'
+	 * Classes to find the difference between two dates in years and two times in
+	 * seconds:
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
